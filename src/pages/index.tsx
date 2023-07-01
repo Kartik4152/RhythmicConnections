195cd8517cb5abf22702c6cafce767f2ae0195ad
Kartik4152 <gaming4152@gmail.com>
2023-07-01 19:29:18 +0530

    feat: Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>RhythmicConnections: Find your perfect harmony.</title>
+    </Head>
+    </>
+  );
+}
